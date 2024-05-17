026bc7b5aca091f167278c387b94e7f18392ceba
faracutrisfa <faracutrisfa@gmail.com>
2024-05-17 14:00:32 +0700

    style: add styling headline

--- src/components/LandingPage/HeadLine.jsx
+++ src/components/LandingPage/HeadLine.jsx
@@ -1,10 +1,18 @@
-import LaptopImage from "../../assets/image/headlineImage.svg"
 import arrow from "../../assets/icon/rightArrow.svg"
+import headlineImage from "../../assets/image/laptop.svg"
 
 const Headline = () => {
     return (
-        <div className="flex flex-col items-center md:flex-row md:items-start">
-            INI HEADLINE
+        <div className="flex flex-col items-center text-secondary-300 md:flex-row md:justify-between md:items-start md:mt-5 lg:mt-10">
+            <img src={headlineImage} alt="image" className="w-64 mb-4 mt-2 md:order-2 md:w-80 xl:w-auto lg:w-96" />
+            <div className="flex flex-col items-center text-center gap-4 xl:gap-7 md:order-1 md:items-start md:text-start">
+                <h1 className="text-2xl font-bold md:text-5xl lg:text-6xl xl:text-7xl sm:text-3xl">Mulai Bisnismu<br />Bersama Kami</h1>
+                <p className="text-xs font-normal w-60 sm:text-base sm:w-96 lg:w-auto lg:text-xl xl:text-2xl">WebMi membantu pembuatan website untuk bisnis Anda agar bisa dikelola darimana saja.</p>
+                <a href="#" className="bg-secondary-50 flex text-primary-500 font-semibold text-xs md:text-xl sm:text-lg xl:text-2xl items-center rounded-xl py-2 px-3 mt-3 md:rounded-2xl lg:rounded-3xl lg:py-3 lg:px-5">
+                    Informasi Lebih Lanjut
+                    <img src={arrow} alt="icon" className="w-3 ml-2 sm:w-5" />
+                </a>
+            </div>
         </div>
     )
 }
